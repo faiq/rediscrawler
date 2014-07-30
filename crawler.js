017d6a6c6f90a7537f7342d6c998511a40c49228
faiq <faiqrazarizvi@gmail.com>
2014-07-30 10:56:14 -0700

    methods not working

--- crawler.js
+++ crawler.js
@@ -6,58 +6,170 @@
 ' use strict '; 
  
 var redis = require('redis')
-  , client = redis.createClient(); 
+  , follow = require('follow')
+  , client = redis.createClient()
 
-function Walker (plist, q, key, dbNum, secs){ 
+Walker.prototype.startFollower = function (){
+  var couchUrl = this.couchUrl
+  var _this = this
+  var settings = 
+  {
+    db: couchUrl
+    , since: 0  //maybe I change this? I dont know 
+    , include_docs: true
+  }  
+  
+  this.follow = follow(settings, function(err, change){ 
+    if(err) console.log(err) 
+    if (change.id){ 
+      _this.addChange(change)   
+     /* client.zscore(this.zKey, change, function(err, res){
+        if (err){
+          console.log('err ' + err)
+          return 
+        }
+        if (!res) console.log(res + ' is falsy') 
+      }) */
+    } 
+  }) 
+}    
+
+Walker.prototype.addChange = function(change){ 
+  var _this = this
+  client.zscore(this.zKey, change, function(err, res){
+    if (err){
+      console.log('err ' + err)
+      return 
+    }
+    if (!res){
+      console.log('yo');
+      client.zadd(_this.zKey, 0, change, function(err, res){
+        if(err){
+          console.log('err ' + err)
+          return
+        } 
+        console.log('added ' + res + ' items.') 
+      })
+    }   
+  }) 
+}
+
+function Walker(couchUrl, zKey){ 
+  this.couchUrl = couchUrl
+  this.zKey = zKey
+} 
+
+var yo =  new Walker('https://skimdb.npmjs.com/registry', 'packages')
+
+yo.startFollower() 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function Walker (plist, lKey, hKey, dbNum, secs){ 
   var _this = this; 
-  this.key = key; 
-  this.secs = secs; 
+  this.hKey = hKey; 
+  if (dbNum > 16){
+    //user passed in secs instead of a dbNum
+    this.secs = dbNum; 
+  }else this.secs = secs;
+  this.lKey = lKey; 
+  this.packages = plist;
 
   if (dbNum){
     client.select(dbNum, function(err){ 
       if(err){ console.log(err); return } 
     }); 
   } 
-  client.exists(key, function(err, exists){
+  client.exists(this.hKey, function(err, exists){
     if(err){ console.log(err); return } 
     if (exists){ 
-      console.log(key);
-      _this.walk(key); 
+      console.log(hKey = " exists");
+      _this.walk(); 
     }else{
       //build index for the first time
-      _this.buildIndex(plist, key);  
+      _this.buildIndex();  
     } 
   });
 }
 
 Walker.prototype.checkStale = function (uTime){ 
   var now = Math.round((new Date()).getTime() / 1000);
-  if (now - uTime >= this.secs) return true 
+  console.log(now - uTime); 
+  if (Math.abs(now - uTime) >= this.secs) return true 
   else return false 
 }  
 
-Walker.prototype.buildIndex = function (packages, key){
+Walker.prototype.buildIndex = function (){
+  var packages = this.packages; 
+  var hKey = this.hKey;
+  var lKey = this.lKey; 
   for(var i = 0; i < packages.length; i++){
     var p = packages[i]; 
     console.log(p); 
-    client.hset(key, p, 0, redis.print)
+    client.hset(hKey, p, 0, redis.print); 
+    client.sadd(lKey, p, redis.print); 
    }
 } 
 
-Walker.prototype.walk = function(key){
+Walker.prototype.walk = function(){
+  var hKey = this.hKey; 
+  var lKey = this.lKey;
   var _this = this;  
-  client.hkeys(key, function(err,replies){ 
+  client.hkeys(hKey, function(err,replies){ 
     replies.forEach(function (reply){ 
-      client.hget(key ,reply, function(err, res){
+      console.log(reply); 
+      client.hget(hKey, reply, function(err, res){
         if (err){ console.log(err); return }  
         res = parseInt(res, 10); 
-        if (_this.checkStale(res)) console.log('dis shit stale as hell yo'); 
+        if (_this.checkStale(res)){ 
+          var now = Math.round((new Date()).getTime() / 1000);
+          client.hset(hKey, reply,  now, redis.print);  
+          client.sadd(lKey, reply, redis.print);
+        } 
         else console.log('dis shit aint stale dawg')
       });
     });
+    client.smembers(lKey, function(err, replies){ 
+      if(err) { console.log(err); return } 
+      replies.forEach(function(reply){ 
+        console.log(reply + ' has been added to the work queue'); 
+      });
+    });   
   });
 }
-          
+*/           
 module.exports = Walker    
 
-var yo = new Walker(['express', 'github2es', 'npm2es', 'elasticsearch', 'yodawg'], null, 'faiq', 259200);
