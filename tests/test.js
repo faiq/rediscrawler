06b846b2401202b6431fe893c845047e6a34cb46
faiq <faiqrazarizvi@gmail.com>
2014-08-01 22:00:08 +0000

    start mockin redis ya bish

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,13 @@
 var lab = require('lab')
   , couch2redis = require('../index')
+  , exec = require('child_process').exec
+  , redis = require('redis') 
+  , experiment = lab.experiment
   , describe = lab.describe 
   , it = lab.it
   , before = lab.before 
   , expect = lab.expect
-
+/*
 describe('the constructor', function (){ 
   it('needs a couch url', function(done){ 
     expect(noUrl).to.throw(Error) 
@@ -26,5 +29,17 @@ describe('the constructor', function (){
       var c2r = new couch2redis('yo', 'yo must be everywhere', 'doesnt have seq with a . b4 it')
     } 
     done()
+  }) 
+}) 
+*/
+
+experiment('redis functionallity' , { timeout: 8000 },  function (){ 
+  before(function (done){ 
+    exec('redis-server', function (err, sto, ste){
+      if(err) console.error(err) 
+      if (sto) console.log('sto: ' + sto) 
+      if (ste) console.log('ste: ' + ste)
+     }) 
+    done()  
   }) 
 }) 
