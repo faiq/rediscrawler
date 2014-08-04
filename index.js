8c51269577049992e7854222761f2464d59afded
faiq <faiqrazarizvi@gmail.com>
2014-08-04 21:55:16 +0000

    cleaning code

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ Couch2Redis.prototype.startFollower = function (opts){
   else followfunc = follow  
   this.follow = followfunc(settings, function(err, change){ 
     if(err) console.error(err) 
-    console.log(change) 
     if (change.id){ 
       _this.addChange(change)
       _this.s.save(change.seq)     
@@ -35,17 +34,16 @@ Couch2Redis.prototype.startFollower = function (opts){
   
 Couch2Redis.prototype.addChange = function(change){ 
   var _this = this
-  console.log('adding change') 
   _this.follow.pause()
   client.zscore(this.zKey, change.id, function(err, res){
     if (err){
-      console.log('err ' + err)
+      console.error('err ' + err)
       return 
     }
     if (!res){
       client.zadd(_this.zKey, 0, change.id, function(err, res){
         if(err){
-          console.log('err ' + err)
+          console.error('err ' + err)
           return
         } 
         console.log('added ' + res + ' items.') 
@@ -68,13 +66,11 @@ function Couch2Redis(couchUrl, zKey, sfPath){
   this.zKey = zKey
   this.s = new SF(sfPath)
   var _this = this  
-  if (fs.existsSync(sfPath)){ //, function (exists) {
-    //if (exists){
+  if (fs.existsSync(sfPath)){ 
       var data = fs.readFileSync(sfPath, 'UTF-8')
       data = parseInt(data, 10)  
       _this.since = data 
   }else _this.since = 0 
-//  })
 } 
 
 module.exports = Couch2Redis 
